9f9bd905303e109b3cb82d8b545f40a68bddbdf8
Matthew Kendon <mkendon@gmail.com>
2015-02-11 16:36:10 +0000

    added new EventSubscriber

---
+++ Form/EventSubscriber/WPFormEventSubscriber.php
@@ -0,0 +1,309 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: outlander
+ * Date: 11/02/2015
+ * Time: 15:33
+ */
+
+namespace Outlandish\OowpSearchBundle\Form\EventSubscriber;
+
+use Outlandish\OowpSearchBundle\Form\Type\OrderByType;
+use Outlandish\OowpSearchBundle\Form\Type\OrderType;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormEvent;
+
+class WPFormEventSubscriber implements EventSubscriberInterface {
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FormEvents::SUBMIT   => 'onSubmit',
+        );
+    }
+
+    public function onSubmit(FormEvent $event)
+    {
+        $data = $event->getData();
+
+        /** @var Form[] $orderFields */
+        $orderFields = $this->getOrderFields($event);
+        $this->addOrderFieldsToData($event, $orderFields, $data);
+
+        /** @var Form[] $orderByFields */
+        $orderByFields = $this->getOrderByFields($event);
+        $this->addOrderByFieldsToData($orderByFields, $data);
+
+        $data = $event->getData();
+
+        /** @var Form[] $fields */
+        $postToPostFields = $this->getPostToPostFields($event);
+        $this->addPostToPostFieldsToData($postToPostFields, $data);
+
+        $fields = $this->getCustomFieldFields($event);
+
+        $this->addCustomFieldFieldsToData($event, $fields, $data);
+
+    }
+
+    /**
+     * @param FormEvent $event
+     * @return array
+     */
+    private function getOrderFields(FormEvent $event)
+    {
+        return array_filter($event->getForm()->all(), function ($field) {
+            return $field instanceof Form ? $field->getConfig()->getType()->getInnerType() instanceof OrderType : false;
+        });
+    }
+
+    /**
+     * @param Form[] $fields
+     * @param array $data
+     */
+    private function addOrderFieldsToData(array $fields, array &$data)
+    {
+        foreach ($fields as $field) {
+            $data = array_merge($data, ['order' => $field->getData()]);
+        }
+    }
+
+    /**
+     * @param FormEvent $event
+     * @return array
+     */
+    private function getOrderByFields(FormEvent $event)
+    {
+        return array_filter($event->getForm()->all(), function ($field) {
+            return $field instanceof Form ? $field->getConfig()->getType()->getInnerType() instanceof OrderByType : false;
+        });
+    }
+
+    /**
+     * @param Form[] $fields
+     * @param array $data
+     */
+    private function addOrderByFieldsToData(array $fields, array &$data)
+    {
+        foreach ($fields as $field) {
+            $value = $field->getData();
+
+            if ($this->isWordPressOrderBy($value)) {
+                $orderby = ['order_by' => $value];
+            } else {
+                $orderby = ['order_by' => 'meta_value', 'meta_key' => $value];
+            }
+            $data = array_merge($data, $orderby);
+        }
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    private function isWordPressOrderBy($value)
+    {
+        return in_array($value, OrderByType::getProtectedValues());
+    }
+
+    /**
+     * @param FormEvent $event
+     * @return array
+     */
+    private function getPostToPostFields(FormEvent $event)
+    {
+        return array_filter($event->getForm()->all(), function ($field) {
+            return $field instanceof Form ? $field->getConfig()->getType()->getInnerType() instanceof PostToPostType : false;
+        });
+    }
+
+    /**
+     * @param Form[] $fields
+     * @param array $data
+     */
+    private function addPostToPostFieldsToData(array $fields, array &$data)
+    {
+
+        $connectedItems = array();
+        $connectedTypes = array();
+
+        foreach ($fields as $field) {
+            $items = $field->getNormData();
+
+            if ($this->nothingSelected($items))
+                continue;
+
+            $this->ensureArray($items);
+
+            $this->addPostIdsToConnectedItems($connectedItems, $items);
+
+            $this->updateConnectedTypes($data, $field, $connectedTypes);
+
+        }
+
+        $this->AddConnectedQueryToData($data, $connectedTypes, $connectedItems);
+    }
+
+    /**
+     * @param $items
+     * @return bool
+     */
+    private function nothingSelected($items)
+    {
+        return empty($items);
+    }
+
+    /**
+     * @param $items
+     */
+    private function ensureArray(&$items)
+    {
+        if (!is_array($items))
+            $items = array($items);
+    }
+
+    /**
+     * @param $connectedItems
+     * @param $items
+     */
+    private function addPostIdsToConnectedItems(&$connectedItems, $items)
+    {
+        $connectedItems = array_merge($connectedItems, array_map(function ($item) {
+            return $item->ID;
+        }, $items));
+    }
+
+    /**
+     * @param $field
+     * @return mixed
+     */
+    private function getPostTypeOfField($field)
+    {
+        return $field->getConfig()->getOption('post_type');
+    }
+
+    /**
+     * @param array $data
+     * @param Form $field
+     * @param array $connectedTypes
+     * @return array
+     */
+    private function updateConnectedTypes(array &$data, Form $field, array &$connectedTypes)
+    {
+        $childPostType = $this->getPostTypeOfField($field);
+        foreach ($data['post_type'] as $postType) {
+            $connectedTypes[] = $this->generateConnectedType($postType, $childPostType);
+        }
+    }
+
+    /**
+     * @param $postType
+     * @param $childPostType
+     * @return string
+     */
+    private function generateConnectedType($postType, $childPostType)
+    {
+        $types = [$postType, $childPostType];
+        sort($types);
+        $connectedType = implode('_', $types);
+        return $connectedType;
+    }
+
+    /**
+     * @param $connectedTypes
+     * @param $connectedItems
+     * @return bool
+     */
+    private function hasConnectedPosts($connectedTypes, $connectedItems)
+    {
+        return !empty($connectedTypes) && !empty($connectedItems);
+    }
+
+    /**
+     * @param array $data
+     * @param $connectedTypes
+     * @param $connectedItems
+     * @return array
+     */
+    private function AddConnectedQueryToData(array &$data, $connectedTypes, $connectedItems)
+    {
+        if ($this->hasConnectedPosts($connectedTypes, $connectedItems)) {
+            $postToPost = [
+                'connection_type' => array_unique($connectedTypes),
+                'connection_items' => array_unique($connectedItems)
+            ];
+            $data = array_merge($data, $postToPost);
+        }
+    }
+
+    /**
+     * @param FormEvent $event
+     * @return array
+     */
+    private function getCustomFieldFields(FormEvent $event)
+    {
+        return array_filter($event->getForm()->all(), function ($field) {
+            return $field instanceof Form ? $field->getConfig()->getType()->getInnerType() instanceof AbstractCustomFieldType : false;
+        });
+    }
+
+    /**
+     * @param $fields
+     * @param $data
+     */
+    private function addCustomFieldFieldsToData($fields, &$data)
+    {
+        $metaQuery = array();
+
+        foreach ($fields as $field) {
+            $value = $field->getData();
+            if ($value) {
+                $metaQuery[] = $this->createSubMetaQuery($field, $value);
+            }
+        }
+        $this->addRelationToMetaQuery($metaQuery);
+        $this->addMetaQueryToData($data, $metaQuery);
+    }
+
+    /**
+     * @param $field
+     * @param $value
+     * @return array
+     */
+    private function createSubMetaQuery($field, $value)
+    {
+        $key = $field->getConfig()->getOption('meta_key');
+        $compare = $field->getConfig()->getOption('compare');
+        $type = $field->getConfig()->getOption('type');
+
+        return array(
+            'key' => $key,
+            'value' => $value,
+            'compare' => $compare,
+            'type' => $type
+        );
+    }
+
+    /**
+     * @param $metaQuery
+     */
+    private function addRelationToMetaQuery(&$metaQuery)
+    {
+        if (count($metaQuery) > 1) {
+            $metaQuery['relation'] = 'AND';
+        }
+    }
+
+    /**
+     * @param $data
+     * @param $metaQuery
+     */
+    private function addMetaQueryToData(&$data, $metaQuery)
+    {
+        if (!empty($metaQuery)) {
+            $data = array_merge($data, ['meta_query' => $metaQuery]);
+        }
+    }
+
+}
--- Form/Type/WPQueryType.php
+++ Form/Type/WPQueryType.php
@@ -10,6 +10,7 @@ namespace Outlandish\OowpSearchBundle\Form\Type;
 
 
 use Outlandish\OowpBundle\Manager\PostManager;
+use Outlandish\OowpSearchBundle\Form\EventSubscriber\WPFormEventSubscriber;
 use Outlandish\RoutemasterBundle\Manager\QueryManager;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\ChoiceList\ObjectChoiceList;
@@ -19,7 +20,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormEvent;
 
-class WPQueryType extends AbstractType {
+abstract class WPQueryType extends AbstractType {
 
     /**
      * @var PostManager
@@ -39,154 +40,19 @@ class WPQueryType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('order', 'order')
-            ->add('order_by', 'order_by')
-            ->add('post_type', 'post_type')
-            ->add('blog', 'post_to_post', array(
-                'post_type' => 'blog',
-                'required'    => false,
-                'choice_list' => new ObjectChoiceList($this->queryManager->query(array('post_type' => 'blog'))->posts, 'post_title')
-            ))
-            ->add('number', 'number_custom_field', array('meta_key' => 'number'))
-            ->add('save', 'submit');
+//        $builder
+//            ->add('order', 'order')
+//            ->add('order_by', 'order_by')
+//            ->add('post_type', 'post_type')
+//            ->add('blog', 'post_to_post', array(
+//                'post_type' => 'blog',
+//                'required'    => false,
+//                'choice_list' => new ObjectChoiceList($this->queryManager->query(array('post_type' => 'blog'))->posts, 'post_title')
+//            ))
+//            ->add('number', 'number_custom_field', array('meta_key' => 'number'))
+//            ->add('save', 'submit');
 
-        $builder->addEventListener(FormEvents::SUBMIT, function(FormEvent $event) {
-            $this->handlePostToPostTypes($event);
-            $this->handleOrderByType($event);
-            $this->handleOrderType($event);
-            $this->handleCustomFieldType($event);
-        });
-    }
-
-    protected function handleOrderType(FormEvent $event)
-    {
-        $data = $event->getData();
-
-        /** @var Form[] $fields */
-        $fields = array_filter($event->getForm()->all(), function($field){
-            return $field instanceof Form ? $field->getConfig()->getType()->getInnerType() instanceof OrderType : false ;
-        });
-
-        foreach($fields as $field){
-            $event->setData(array_merge(
-                $data,
-                array('order' => $field->getData())
-            ));
-        }
-    }
-
-    /**
-     * @param FormEvent $event
-     */
-    protected function handleCustomFieldType(FormEvent $event)
-    {
-        $data = $event->getData();
-
-        /** @var Form[] $fields */
-        $fields = array_filter($event->getForm()->all(), function($field){
-            return $field instanceof Form ? $field->getConfig()->getType()->getInnerType() instanceof AbstractCustomFieldType : false ;
-        });
-
-        $metaQuery = array();
-
-        foreach($fields as $field){
-            $value = $field->getData();
-            if($value){
-                $metaKey = $field->getConfig()->getOption('meta_key');
-                $compare = $field->getConfig()->getOption('compare');
-                $type = $field->getConfig()->getOption('type');
-
-                $metaQuery[] = array(
-                    'key' => $metaKey,
-                    'value' => $value,
-                    'compare' => $compare,
-                    'type' => $type
-                );
-            }
-        }
-
-        if(!empty($metaQuery)){
-            $event->setData(array_merge(
-                $data,
-                array('meta_query' => $metaQuery)
-            ));
-        }
-
-    }
-
-    protected function handleOrderByType(FormEvent $event)
-    {
-        $data = $event->getData();
-
-        /** @var Form[] $fields */
-        $fields = array_filter($event->getForm()->all(), function($field){
-            return $field instanceof Form ? $field->getConfig()->getType()->getInnerType() instanceof OrderByType : false ;
-        });
-
-        foreach($fields as $field){
-            $value = $field->getData();
-
-            //if the fieldData is in the protected values, then set the order_by to the $value
-            //else set order_by to meta_value, and meta_key to the $value
-            if(in_array($value, OrderByType::getProtectedValues())){
-                $event->setData(array_merge(
-                    $data,
-                    array('order_by' => $value)
-                ));
-            } else {
-                $event->setData(array_merge(
-                    $data,
-                    array(
-                        'order_by' => 'meta_value',
-                        'meta_key' => $value
-                    )
-                ));
-            }
-        }
-    }
-
-    protected function handlePostToPostTypes(FormEvent $event)
-    {
-        $data = $event->getData();
-
-        $connectedItems = array();
-        $connectedTypes = array();
-
-        /** @var Form[] $fields */
-        $fields = array_filter($event->getForm()->all(), function($field){
-            return $field instanceof Form ? $field->getConfig()->getType()->getInnerType() instanceof PostToPostType : false ;
-        });
-
-        foreach($fields as $field){
-            //if we have no items, ignore this field
-            //if $items is not array turn it into one
-            //items should be an array of Posts
-            $items = $field->getNormData();
-            if(empty($items)) continue;
-            if(!is_array($items)) $items = array($items);
-
-            //add the ids of each of the connected post items to the $connectedItems array
-            $connectedItems = array_merge($connectedItems, array_map(function($item){ return $item->ID; },$items));
-
-            $childPostType = $field->getConfig()->getOption('post_type');
-
-            //foreach of the post types that we will be searching for, create a connectedType string
-            foreach($data['post_type'] as $postType){
-                $types = array($postType, $childPostType);
-                sort($types);
-                $connectedTypes[] = implode('_', $types);
-            }
-
-            array_unique($connectedTypes);
-        }
-
-        if(!empty($connectedTypes) && !empty($connectedTypes)){
-            $event->setData(array_merge($data, array(
-                'connection_type' => $connectedTypes,
-                'connection_items' => $connectedItems
-            )));
-        }
+        $builder->addEventSubscriber(new WPFormEventSubscriber());
     }
 
     public function getParent()
